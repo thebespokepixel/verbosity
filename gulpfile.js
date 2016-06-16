b61bfada3539b6cc0292f96008d3d09427c6b69a
Mark Griffiths <mark@thebespokepixel.com>
2016-06-16 16:59:03 +0100

    Snapshot verbosity 0.7.0-∆005

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,6 @@ const cordial = require('@thebespokepixel/cordial')()
 
 // transpilation/formatting
 gulp.task('bundle', cordial.macro({
-	source: 'src/index.es6'
-}).bundle())
-
-gulp.task('master', cordial.macro({
 	master: true,
 	source: 'src/index.es6'
 }).bundle())
@@ -21,8 +17,8 @@ gulp.task('ava', cordial.test().ava(['test/*.js']))
 gulp.task('xo', cordial.test().xo(['src/*.es6']))
 gulp.task('test', gulp.parallel('xo', 'ava'))
 
-gulp.task('post-flow-release-start', gulp.series('reset', 'master', 'version-release'))
-gulp.task('post-flow-release-finish', gulp.series('test', 'publish', 'push-force'))
+// Hooks
+gulp.task('finish-release', gulp.series('push-force'))
 
 // Default
 gulp.task('default', gulp.series('bump', 'bundle'))
